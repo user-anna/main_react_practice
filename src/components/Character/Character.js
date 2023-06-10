eb0979e46fb71e8902fa69eaf781d1362bc1a48f
user-anna <okunura71@gmail.com>
2023-06-10 17:45:10 +0300

    Rick and Morti

---
+++ src/components/Character/Character.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Character = ({character}) => {
+    const {id, name, status, species, image} = character;
+    return (
+        <div>
+            <div>id: {id}</div>
+            <div>name: {name}</div>
+            <div>status: {status}</div>
+            <div>species: {species}</div>
+            <img src={image} alt={name}/>
+
+        </div>
+    );
+};
+
+export default Character;
